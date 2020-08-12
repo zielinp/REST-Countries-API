d3cd10cd2ede2d23a427560aec1d2a7271556cab
Paulina Zielinska <pzielinska.zielona@gmail.com>
2020-08-12 12:17:41 +0200

    Created country_card component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
+import CountryCard from "./country_card"
+
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return( <div>
+          <CountryCard 
+          flagUrl = "https://upload.wikimedia.org/wikipedia/commons/3/3d/Flag_of_germany_800_480.png"
+          countryName = 'Germany'
+          countryPopulation = '123.456.789'
+          countryRegion = 'Europe'
+          countryCapital = 'Berlin' />
+          </div>)
+
 }
